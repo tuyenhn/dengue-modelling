942b87598c774167ef737f6005966648679fc197
tuyenhn <hntuyen282@gmail.com>
2023-11-06 13:48:48 +0700

    Update data_weekly.R

--- data_weekly.R
+++ data_weekly.R
@@ -1,10 +1,10 @@
-library(tidyverse)
-library(tsibble)
+# library(tidyverse)
+# library(tsibble)
+#
+# library(sf)
+# library(stars)
 
-library(sf)
-library(stars)
-
-# incidence data
+## incidence data
 incidence_raw <- read_csv("incidence_ts_in.csv", show_col_types = FALSE) %>%
   mutate(date_admitted = as.Date(date_admitted)) %>%
   as_tsibble(index = date_admitted) %>%
@@ -16,77 +16,90 @@ incidence_weekly_df <- incidence_raw %>%
   rename(date_admitted = agg)
 
 ## sets
-train_weekly_df <- incidence_weekly_df %>%
-  filter_index("2000 W01" ~ "2017 W52")
+# train_weekly_df <- incidence_weekly_df %>%
+#   filter_index("2000 W01" ~ "2017 W52")
+#
+# val_weekly_df <- incidence_weekly_df %>%
+#   filter_index("2018 W01" ~ "2018 W52")
+#
+# test_weekly_df <- incidence_weekly_df %>%
+#   filter_index("2019 W01" ~ "2019 W52")
 
-val_weekly_df <- incidence_weekly_df %>%
-  filter_index("2018 W01" ~ "2018 W52")
+incidence_weekly_df <- read_rds("./data_weekly_rds-s/incidence_weekly_df.rds")
+train_weekly_df <- read_rds("./data_weekly_rds-s/train_weekly_df.rds")
+val_weekly_df <- read_rds("./data_weekly_rds-s/val_weekly_df.rds")
+test_weekly_df <- read_rds("./data_weekly_rds-s/test_weekly_df.rds")
 
-test_weekly_df <- incidence_weekly_df %>%
-  filter_index("2019 W01" ~ "2019 W52")
+## HCMC shapefile
+# hcmc_shp <- read_rds("gadm/gadm41_VNM_1_pk.rds") %>%
+#   terra::unwrap() %>%
+#   st_as_sf() %>%
+#   filter(GID_1 == "VNM.25_1")
 
-# HCMC shapefile
-hcmc_shp <- read_rds("gadm/gadm41_VNM_1_pk.rds") %>%
-  terra::unwrap() %>%
-  st_as_sf() %>%
-  filter(GID_1 == "VNM.25_1")
+# hcmc_shp_w_buff <- st_buffer(hcmc_shp, units::set_units(10, "km"))
 
-hcmc_shp_w_buff <- st_buffer(hcmc_shp, units::set_units(10, "km"))
+## weather variables from ERA5
+# raw_weather <- read_ncdf("L2_V3_T2m_RH_TP_SH_1999_2022.nc") %>%
+#   st_set_crs(4326)
 
-# temperature and precipitation from ERA5
-raw_weather <- read_ncdf("L2_V3_T2m_RH_TP_SH_1999_2022.nc") %>%
-  st_set_crs(4326)
+# hcmc_temp_df <- raw_weather %>%
+#   select(t2m) %>%
+#   aggregate("1 week", FUN = mean) %>%
+#   aggregate(hcmc_shp_w_buff, FUN = mean) %>%
+#   as_tibble() %>%
+#   select(time, t2m) %>%
+#   rename(date = time) %>%
+#   mutate(
+#     date = as.Date(date) %>% yearweek(),
+#     t2m = t2m - 273.15,
+#   ) %>%
+#   as_tsibble()
 
-hcmc_temp_df <- raw_weather %>%
-  select(t2m) %>%
-  aggregate("1 week", FUN = mean) %>%
-  aggregate(hcmc_shp_w_buff, FUN = mean) %>%
-  as_tibble() %>%
-  select(time, t2m) %>%
-  rename(date = time) %>%
-  mutate(
-    date = as.Date(date) %>% yearweek(),
-    t2m = t2m - 273.15,
-  ) %>%
-  as_tsibble()
+# hcmc_precip_df <- raw_weather %>%
+#   select(tp) %>%
+#   aggregate("1 week", FUN = sum) %>%
+#   aggregate(hcmc_shp_w_buff, FUN = sum) %>%
+#   as_tibble() %>%
+#   select(time, tp) %>%
+#   rename(date = time, precip = tp) %>%
+#   mutate(
+#     date = as.Date(date) %>% yearweek(),
+#     precip = precip * 1000,
+#   ) %>%
+#   as_tsibble()
 
-hcmc_precip_df <- raw_weather %>%
-  select(tp) %>%
-  aggregate("1 week", FUN = sum) %>%
-  aggregate(hcmc_shp_w_buff, FUN = sum) %>%
-  as_tibble() %>%
-  select(time, tp) %>%
-  rename(date = time, precip = tp) %>%
-  mutate(
-    date = as.Date(date) %>% yearweek(),
-    precip = precip * 1000,
-  ) %>%
-  as_tsibble()
+# hcmc_rh_df <- raw_weather %>%
+#   select(rh) %>%
+#   aggregate("1 week", FUN = mean) %>%
+#   aggregate(hcmc_shp_w_buff, FUN = mean) %>%
+#   as_tibble() %>%
+#   select(time, rh) %>%
+#   rename(date = time, rh = rh) %>%
+#   mutate(date = as.Date(date) %>% yearweek()) %>%
+#   as_tsibble()
 
-hcmc_rh_df <- raw_weather %>%
-  select(rh) %>%
-  aggregate("1 week", FUN = mean) %>%
-  aggregate(hcmc_shp_w_buff, FUN = mean) %>%
-  as_tibble() %>%
-  select(time, rh) %>%
-  rename(date = time, rh = rh) %>%
-  mutate(date = as.Date(date) %>% yearweek()) %>%
-  as_tsibble()
+hcmc_temp_df <- read_rds("./data_weekly_rds-s/hcmc_temp_df.rds")
+hcmc_precip_df <- read_rds("./data_weekly_rds-s/hcmc_precip_df.rds")
+hcmc_rh_df <- read_rds("./data_weekly_rds-s/hcmc_rh_df.rds")
 
-hcmc_weather_df <- hcmc_temp_df %>%
-  left_join(hcmc_precip_df) %>%
-  left_join(hcmc_rh_df)
+# hcmc_weather_df <- hcmc_temp_df %>%
+#   left_join(hcmc_precip_df) %>%
+#   left_join(hcmc_rh_df)
 
-# join weather and incidence data
-incidence_weekly_weather_df <- incidence_weekly_df %>%
-  append_row(n = -51, keep_all = TRUE) %>%
-  left_join(hcmc_weather_df, by = c("date_admitted" = "date"))
+## join weather and incidence data
+# incidence_weekly_weather_df <- incidence_weekly_df %>%
+#   append_row(n = -51, keep_all = TRUE) %>%
+#   left_join(hcmc_weather_df, by = c("date_admitted" = "date"))
 
-train_weekly_weather_df <- incidence_weekly_weather_df %>%
-  filter_index("2000 W01" ~ "2017 W52")
+# train_weekly_weather_df <- incidence_weekly_weather_df %>%
+#   filter_index("2000 W01" ~ "2017 W52")
+#
+# val_weekly_weather_df <- incidence_weekly_weather_df %>%
+#   filter_index("2018 W01" ~ "2018 W52")
+#
+# test_weekly_weather_df <- incidence_weekly_weather_df %>%
+#   filter_index("2019 W01" ~ "2019 W52")
 
-val_weekly_weather_df <- incidence_weekly_weather_df %>%
-  filter_index("2018 W01" ~ "2018 W52")
-
-test_weekly_weather_df <- incidence_weekly_weather_df %>%
-  filter_index("2019 W01" ~ "2019 W52")
+train_weekly_weather_df <- read_rds("./data_weekly_rds-s/train_weekly_weather_df.rds")
+val_weekly_weather_df <- read_rds("./data_weekly_rds-s/val_weekly_weather_df.rds")
+test_weekly_weather_df <- read_rds("./data_weekly_rds-s/test_weekly_weather_df.rds")
